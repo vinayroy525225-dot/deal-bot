07eb9e2751fddfa3c1a7ab1942eef835f2b95d68
Vinay <vinayroy525225@gmail.com>
2026-05-10 00:36:58 +0530

    telegram test

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import requests
 BOT_TOKEN = "8622451849:AAFl4jjC2mr5rf2671SA3F4g3vIFcqEYwGs"
 CHAT_ID = "5044500645"
 
-message = "🔥 Railway Bot Working Successfully!"
+message = "🔥 Railway Bot Workings Successfully!"
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 

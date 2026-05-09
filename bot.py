23add64214fc8c9147d594c17e7157e57b778e78
Vinay <vinayroy525225@gmail.com>
2026-05-09 23:55:26 +0530

    removed selenium

--- bot.py
+++ bot.py
@@ -1,10 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-
 import requests
+from bs4 import BeautifulSoup
 import time
 
 BOT_TOKEN = "8622451849:AAFl4jjC2mr5rf2671SA3F4g3vIFcqEYwGs"
@@ -14,51 +9,47 @@ URL = "https://www.amazon.in/Oppo-K13-5G-Prism-Storage/dp/B0F8W2D943/522-6651414
 
 TARGET_PRICE = 18500
 
-chrome_options = Options()
-
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
-
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()),
-    options=chrome_options
-)
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
 
 while True:
 
     print("Checking Price...")
 
-    driver.get(URL)
+    page = requests.get(URL, headers=headers)
 
-    time.sleep(5)
+    soup = BeautifulSoup(page.content, "html.parser")
 
-    price = driver.find_element(By.CLASS_NAME, "a-price-whole")
+    price = soup.find("span", class_="a-price-whole")
 
-    current_price = int(price.text.replace(",", ""))
+    if price:
 
-    print("CURRENT PRICE:")
-    print(current_price)
+        current_price = int(price.text.replace(",", "").replace(".", ""))
 
-    if current_price <= TARGET_PRICE:
+        print("CURRENT PRICE:", current_price)
 
-        message = f"🔥 Deal Alert!\nPrice Dropped: ₹{current_price}\n{URL}"
+        if current_price <= TARGET_PRICE:
 
-        send_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+            message = f"🔥 Deal Alert!\nPrice: ₹{current_price}\n{URL}"
 
-        data = {
-            "chat_id": CHAT_ID,
-            "text": message
-        }
+            send_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-        requests.post(send_url, data=data)
+            data = {
+                "chat_id": CHAT_ID,
+                "text": message
+            }
 
-        print("TELEGRAM ALERT SENT")
+            requests.post(send_url, data=data)
+
+            print("TELEGRAM ALERT SENT")
+
+        else:
+            print("Price Still High")
 
     else:
-        print("Price Still High")
+        print("Price Not Found")
 
-    print("Checking again in 60 seconds...")
+    print("Checking again in 60 sec...\n")
 
     time.sleep(60)

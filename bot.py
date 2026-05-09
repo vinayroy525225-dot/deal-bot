b2acedfe934fbbcc1cdea0401d48e9528697aa89
Vinay <vinayroy525225@gmail.com>
2026-05-09 23:48:32 +0530

    fixed selenium

--- bot.py
+++ bot.py
@@ -1,91 +1,64 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
 import requests
 import time
 
 BOT_TOKEN = "8622451849:AAFl4jjC2mr5rf2671SA3F4g3vIFcqEYwGs"
 CHAT_ID = "5044500645"
 
-SEARCH_URL = "https://www.bing.com/search?q=MOBILE+UNDER+5000&form=ANNTH1&refig=69ff69c7742d49b3ae8962ae79a76746&pc=W251"
+URL = "https://www.amazon.in/Oppo-K13-5G-Prism-Storage/dp/B0F8W2D943/522-6651414-9906254?pd_rd_w=cFCWN&content-id=amzn1.sym.d1406b44-aa69-47e4-9270-f613e12d52dc&pf_rd_p=d1406b44-aa69-47e4-9270-f613e12d52dc&pf_rd_r=F94RZH9YNVERVG9K292M&pd_rd_wg=kUT2o&pd_rd_r=33bea165-9c6e-4265-8fef-87d9a69fa1e2&pd_rd_i=B0F8W2D943&psc=1"
 
-MAX_PRICE = 5000
+TARGET_PRICE = 18500
 
-def send_telegram(message):
+chrome_options = Options()
 
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
-    data = {
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-
-    requests.post(url, data=data)
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install())
+    service=Service(ChromeDriverManager().install()),
+    options=chrome_options
 )
-
-sent_products = set()
 
 while True:
 
-    print("\nChecking deals...\n")
+    print("Checking Price...")
 
-    driver.get(SEARCH_URL)
+    driver.get(URL)
 
     time.sleep(5)
 
-    products = driver.find_elements(
-        By.XPATH,
-        '//div[@data-component-type="s-search-result"]'
-    )
+    price = driver.find_element(By.CLASS_NAME, "a-price-whole")
 
-    for item in products:
+    current_price = int(price.text.replace(",", ""))
 
-        try:
+    print("CURRENT PRICE:")
+    print(current_price)
 
-            title = item.find_element(
-                By.XPATH,
-                './/h2'
-            ).text
+    if current_price <= TARGET_PRICE:
 
-            price = item.find_element(
-                By.XPATH,
-                './/span[contains(@class,"a-price-whole")]'
-            ).text
+        message = f"🔥 Deal Alert!\nPrice Dropped: ₹{current_price}\n{URL}"
 
-            link = item.find_element(
-                By.XPATH,
-                './/a'
-            ).get_attribute("href")
+        send_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-            price = int(price.replace(",", ""))
+        data = {
+            "chat_id": CHAT_ID,
+            "text": message
+        }
 
-            print(title)
-            print(price)
+        requests.post(send_url, data=data)
 
-            if price <= MAX_PRICE:
+        print("TELEGRAM ALERT SENT")
 
-                if link not in sent_products:
+    else:
+        print("Price Still High")
 
-                    message = (
-                        f"🔥 MOBILE DEAL FOUND 🔥\n\n"
-                        f"{title}\n\n"
-                        f"Price: ₹{price}\n\n"
-                        f"{link}"
-                    )
-
-                    send_telegram(message)
-
-                    print("ALERT SENT")
-
-                    sent_products.add(link)
-
-        except:
-            pass
-
-    print("\nWaiting 60 seconds...\n")
+    print("Checking again in 60 seconds...")
 
     time.sleep(60)
